aa17ce22124b346f298233ac9634dd2ba5bbd46f
Arda Akdogan <osmanardakdgn@gmail.com>
2025-09-29 14:48:11 +0300

    init module four our package

---
+++ src/penguins_data_lab/__init__.py
@@ -0,0 +1,6 @@
+"""penguins_data_lab package"""
+from .module_data_loader import DataLoaderCleaner
+from .module_analyzer import Analyzer
+from .module_visualizer import Visualizer
+
+__all__ = ["DataLoaderCleaner", "Analyzer", "Visualizer"]
